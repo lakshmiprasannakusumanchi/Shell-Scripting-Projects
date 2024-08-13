0b168b0b9c1fca684b72a24296ba2092d8d4b7b7
Lakshmi prasanna Kusumanchi <118244809+lakshmiprasannakusumanchi@users.noreply.github.com>
2024-08-13 19:21:56 +0530

    Update proj1.sh

--- proj1.sh
+++ proj1.sh
@@ -1,5 +1,6 @@
 # Monitoring Free RAM Space of a directory
-# The script monitors the available free RAM space by using the `free` command and filtering the output with `grep` and `awk` to extract the available memory in megabytes. It then compares the free space against a predefined limit (400 MB). If the available space is below the limit, it prompts a warning message to free up space; otherwise, it displays the current free RAM.
+# The script monitors the available free RAM space by using the `free` command and filtering the output with `grep` and `awk` to extract the available memory in megabytes. 
+# It then compares the free space against a predefined limit (400 MB). If the available space is below the limit, it prompts a warning message to free up space; otherwise, it displays the current free RAM.
 
 #!/bin/bash
 space=$(free -mt | grep "Total" | awk '{print$4}')
